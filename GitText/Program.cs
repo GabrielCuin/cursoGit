d737b564bc6701fa2f3a92f6e4c37c22694eb92e
Gabriel Cuin <gabrielcuinprof@gmail.com>
2020-07-14 20:42:29 -0300

    Acrescentado a mensagem bom dia

--- GitText/Program.cs
+++ GitText/Program.cs
@@ -7,6 +7,7 @@ namespace GitText
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia"); 
             Console.WriteLine();
         }
     }
